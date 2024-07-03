649b69a1ed0ee16db98d47d1b0e25ebb5e30d05b
kitravee <kitravee.swk@gmail.com>
2024-07-03 13:05:15 +0700

    feat: work1 common string prefix

---
+++ work1-longest-common-string-prefix/utils/longest-common-prefix.test.ts
@@ -0,0 +1,23 @@
+import { showLongestCommonPrefix } from "./longest-common-prefix";
+
+export const  runTests = () => {
+  interface TestCase {
+      input: string[];
+      expected: string;
+  }
+
+  let testCases: TestCase[] = [
+      { input: ["flower", "flow", "flight"], expected: "fl" },
+      { input: ["dog", "racecar", "car"], expected: "" },
+      { input: ["interstellar", "internet", "interval"], expected: "inter" },
+      { input: ["throne", "throne", "throne"], expected: "throne" },
+      { input: ["apple", "ape", "april"], expected: "ap" },
+      { input: ["", "b", "c"], expected: "" },
+      { input: ["a"], expected: "a" }
+  ];
+
+  testCases.forEach(({ input, expected }, index) => {
+      let result = showLongestCommonPrefix(input);
+      console.log(`Test case ${index + 1}: ${result === expected ? "Passed" : "Failed"}`);
+  });
+}
---
+++ work1-longest-common-string-prefix/utils/longest-common-prefix.ts
@@ -0,0 +1,23 @@
+export const findShortestString = (strs: string[]) => {
+  return strs.reduce((a: string, b: string) => (a.length <= b.length ? a : b));
+};
+
+export const showLongestCommonPrefix = (strs: string[]) => {
+  if (strs.length === 0) return "";
+
+  const shortestStr = findShortestString(strs);
+
+  for (let i = 0; i < shortestStr.length; i++) {
+    let char = shortestStr[i];
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        const commonPrefix = shortestStr.slice(0, i);
+        return commonPrefix;
+      }
+    }
+  }
+
+  return shortestStr;
+};
+
+
